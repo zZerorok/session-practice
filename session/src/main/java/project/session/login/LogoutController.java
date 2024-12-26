7def99ffbb1ea1eee79a71d404befa2cb7f592e9
youngrok <yr951103@gmail.com>
2024-12-26 17:31:02 +0900

    feat: 로그아웃 시 SessionManager를 통해 세션 삭제 및 쿠키 만료

--- session/src/main/java/project/session/login/LogoutController.java
+++ session/src/main/java/project/session/login/LogoutController.java
@@ -1,18 +1,27 @@
 package project.session.login;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import project.session.session.SessionManager;
 
 @RequiredArgsConstructor
 @Controller
 public class LogoutController {
+    private final SessionManager sessionManager;
 
-    @PostMapping("/logout")
+    //    @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
         expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request) {
+        sessionManager.expireSession(request);
         return "redirect:/";
     }
 
