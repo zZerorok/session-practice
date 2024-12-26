a3227f82939bc308fb8c64a4dfc242b406e2368a
youngrok <yr951103@gmail.com>
2024-12-26 17:19:42 +0900

    feat: 로그인 성공 시 로그인 전용 화면으로 리다이렉트하도록 구현

--- session/src/main/java/project/session/HomeController.java
+++ session/src/main/java/project/session/HomeController.java
@@ -1,13 +1,38 @@
 package project.session;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import project.session.member.Member;
+import project.session.member.MemberRepository;
 
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
+    private final MemberRepository memberRepository;
 
-    @GetMapping("/")
+    //    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(
+            @CookieValue(name = "memberId", required = false) Long memberId,
+            Model model
+    ) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "login-home";
     }
 }
