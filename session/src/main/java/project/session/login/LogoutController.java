578ba7c315e9b5112b3fb25320a4f2425822946d
youngrok <yr951103@gmail.com>
2024-12-26 17:20:59 +0900

    feat: 로그아웃 시 쿠키 삭제되도록 구현

---
+++ session/src/main/java/project/session/login/LogoutController.java
@@ -0,0 +1,24 @@
+package project.session.login;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class LogoutController {
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+}
